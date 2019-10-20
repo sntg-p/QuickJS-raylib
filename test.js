1d5ce6edaafb52e2654ee5d8d000ed2d3314a20f
Santiago Podestá <santiagolp98@gmail.com>
2019-10-20 04:05:23 -0300

    Updated test.js to test some changes

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import * as rl from './qjs-raylib/qjs-raylib.so';
+import { Color } from './qjs-raylib/qjs-raylib.so';
 import * as rlCore from './qjs-raylib/core.js';
 import * as rlTextures from './qjs-raylib/textures.js';
 
@@ -15,19 +15,20 @@ let textSize = 20;
 let image = rlTextures.loadImage('grass_colored.png');
 rlCore.setWindowIcon(image);
 
-let asd = new rl.Vector2(44, 33);
-console.log(`${asd.x} ${asd.y}`)
+const bg = new Color(255, 128, 64);
+const fg = Color(255, 255, 255, 192);
 
 while (!rlCore.windowShouldClose()) {
 	rlCore.beginDrawing();
-	//rlCore.beginDrawing();
 
-	rlCore.drawFps(0,0);
+	rlCore.clearBackground(bg);
+	rlCore.drawFps(2, 0);
+	rlCore.drawText("Welcome to QuickJS+raylib!", 264, textY, textSize, fg);
 	
-	rlCore.clearBackground(0xFFFFFFFF);
-	rlCore.drawText("Congrats! You created your first window!", 190, textY, textSize, 0xC8C8C8FF);
 	textY++;
-	if (textY > screenHeight) textY = -textSize;
+	
+	if (textY > screenHeight)
+		textY = -textSize;
 
 	rlCore.endDrawing();
 }
